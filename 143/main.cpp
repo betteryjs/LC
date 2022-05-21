50cddf42f0a2f4a2a0bf0864d1491bd816d90918
betteryjs <aanbnbnbnb@gmail.com>
2022-05-21 23:13:48 +0800

    add 143

---
+++ 143/main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+
+
+/**
+ * Definition for singly-linked list.*/
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+
+private:
+
+
+    ListNode * getMid(ListNode * head){
+        if(head== nullptr  || head->next== nullptr){
+            return head;
+        }
+        ListNode * fast=head,* slow=head;
+        while (fast!= nullptr && fast->next!= nullptr){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        return slow;
+    }
+
+
+    ListNode * reverse(ListNode * head){
+        if(head== nullptr || head->next== nullptr){
+            return head;
+        }
+        ListNode * prev= nullptr;
+        ListNode * cur=head;
+        while (cur!= nullptr){
+            ListNode * nextCur=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=nextCur;
+        }
+        return prev;
+
+
+    }
+
+
+public:
+    void reorderList(ListNode *head) {
+        if(head== nullptr || head->next== nullptr){
+            return;
+        }
+
+        ListNode * mid= getMid(head);
+        ListNode * head2=mid->next;
+        mid->next= nullptr;
+        ListNode * newHead= reverse(head2);
+        ListNode * dummyHead=new ListNode(-1);
+        ListNode * cur3=dummyHead;
+        ListNode * cur1=head;
+        ListNode * cur2=newHead;
+        while (cur1 != nullptr && cur2!= nullptr){
+
+            cur3->next=cur1;
+            cur1=cur1->next;
+            cur3=cur3->next;
+
+            cur3->next=cur2;
+            cur2=cur2->next;
+            cur3=cur3->next;
+        }
+
+
+
+        cur3->next=cur1 == nullptr ? cur2:cur1;
+
+
+        head=dummyHead->next;
+
+
+
+
+
+
+    }
+};
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
