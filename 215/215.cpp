12d46afadcc1a5e8ce81897be6ed0c1c6161cd53
betteryjs <aanbnbnbnb@gmail.com>
2022-09-27 08:41:50 +0800

    add all

--- 215/215.cpp
+++ 215/215.cpp
@@ -1,236 +1,67 @@
 //
-// Created by yjs on 2022/2/22.
+// Created by yjs on 2022/9/26.
 //
-
 #include <bits/stdc++.h>
-#include <iostream>
+
 
 using namespace std;
 
-
-
-
-
-
-
-
-
-
-
-
-class QuickSort {
-public:
+class Solution {
 public:
 
-    static void pprint(const string str, const vector<int> num) {
-        cout << str << " [ ";
-        for_each(num.begin(), num.end() - 1, [](const int a) { cout << a << " ,"; });
-        cout << *(num.end() - 1) << " ]";
-        cout << endl;
-    }
+    int Partition(vector<int> &arr, int low, int high) {
+        int pivot = arr[low];
+        while (low < high) {
 
-    void maxHeapify(vector<int> &arr, int i, int heapSize) {
-        while (2*i+1<=heapSize){
-            int leftChild=(2*i)+1,rightChild=(2*i)+2,target=leftChild;
-            if(rightChild <=heapSize && arr[rightChild]> arr[leftChild]){
-                target=rightChild;
-            }
-
-
-            if(arr[target] > arr[i]){
-                swap(arr[target],arr[i]);
-
-            }else{
-                break;
-            }
-            i=target;
-
+            while (low < high && arr[high] >= pivot) high--;
+            arr[low] = arr[high];
+            while (low < high && arr[low] <= pivot) low++;
+            arr[high] = arr[low];
 
 
         }
-
-
-    }
-
-    void maxHeapify2(vector<int>& a, int i, int heapSize) {
-        int l = i * 2 + 1, r = i * 2 + 2, largest = i;
-        if (l <= heapSize && a[l] > a[largest]) {
-            largest = l;
-        }
-        if (r <= heapSize && a[r] > a[largest]) {
-            largest = r;
-        }
-        if (largest != i) {
-            swap(a[i], a[largest]);
-            maxHeapify2(a, largest, heapSize);
-        }
-    }
-
-
-
-
-
-
-
-
-
-    void build_heap(vector<int> &arr, int size) {
-        for (int i =  size/2; i >= 0; i--) {
-            maxHeapify2(arr, i, size);
-        }
-    }
-
-    void heap_sort(vector<int> & arr) {
-        int len=arr.size()-1;
-        build_heap(arr, len);
-        for (int i = len ; i >=1; i--) {
-            swap(arr[i], arr[0]);
-            len--;
-            maxHeapify2(arr, 0, len);
-        }
-    }
-
-
-
-
-
-
-
-    void comparator(vector<int> &arr) {
-        sort(arr.begin(), arr.end());
-
-    }
-
-    vector<int> generateRandomArray(int maxSize, int maxValue) {
-
-        uniform_real_distribution<> u(0,1);
-        uniform_int_distribution<unsigned > u2(0,maxValue + 1);
-        default_random_engine e;
-        vector<int> arr(maxSize*u(e));
-        for (int i = 0; i < arr.size(); i++) {
-            arr[i]=u2(e);
-        }
-        return arr;
-
+        arr[low] = pivot;
+        return low;
 
     }
 
 
-    void run() {
-        int testTime = 500000;
-        int maxSize = 100; // 随机数组的长度0～100
-        int maxValue = 100;// 值：-100～100
-        bool succeed = true;
-        for (int i = 0; i < testTime; i++) {
-            vector<int> arr = generateRandomArray(maxSize, maxValue);
+    int randomPartition(vector<int> &arr, int low, int high) {
+        swap(arr[low], arr[low + rand() % (high - low + 1)]);
+        return Partition(arr, low, high);
+    }
 
-            vector<int> arr1(arr);
-            vector<int> arr2(arr);
-            this->heap_sort(arr1);
-            comparator(arr2);
-            if (arr1!=arr2) {
-                // 打印arr1
-                // 打印arr2
-                succeed = false;
-                for (int j = 0; j < arr.size(); j++) {
-                    cout << arr[j] <<  " ";
-                }
-                cout << endl;
-                break;
-            }
+    int quickSelect(vector<int> &arr, int low, int hig, int target) {
+        int p = randomPartition(arr, low, hig);
+        if (p == target) {
+            return target;
+        } else {
+            return p < target ? quickSelect(arr, p + 1, hig, target) :
+                   quickSelect(arr, low, p - 1, target);
+
         }
-        cout << (succeed? "Nice!" : "Fucking fucked!")<<endl;
-        vector<int> arr = generateRandomArray(maxSize, maxValue);
-        pprint("arr before ",arr);
-        this->heap_sort(arr);
-        pprint("arr after",arr);
     }
 
 
-
-
+    int findKthLargest(vector<int> &nums) {
+        return quickSelect(nums, 0, nums.size() - 1, nums.size() / 2);
+    }
 };
-class CheckSort{
-private:
-    function<void(vector<int>&)> functionSort;
-public:
-    CheckSort(const function<void(vector<int>&)> & function ) :functionSort(function){};
-    static void pprint(const string str, const vector<int> num) {
-        cout << str << " [ ";
-        for_each(num.begin(), num.end() - 1, [](const int a) { cout << a << " ,"; });
-        cout << *(num.end() - 1) << " ]";
-        cout << endl;
-    }
-    friend QuickSort;
-
-
-
-
-public:
-
-    void comparator(vector<int> &arr) {
-        sort(arr.begin(), arr.end());
-    }
-
-    vector<int> generateRandomArray(int maxSize, int maxValue) {
-
-        uniform_real_distribution<> u(0,1);
-        uniform_int_distribution<unsigned > u2(0,maxValue + 1);
-        default_random_engine e;
-        vector<int> arr(maxSize*u(e));
-        for (int i = 0; i < arr.size(); i++) {
-            arr[i]=u2(e);
-        }
-        return arr;
-
-
-    }
-
-
-    void run() {
-        int testTime = 500000;
-        int maxSize = 100; // 随机数组的长度0～100
-        int maxValue = 100;// 值：-100～100
-        bool succeed = true;
-        for (int i = 0; i < testTime; i++) {
-            vector<int> arr = generateRandomArray(maxSize, maxValue);
-
-            vector<int> arr1(arr);
-            vector<int> arr2(arr);
-            this->functionSort(arr1);
-            comparator(arr2);
-            if (arr1!=arr2) {
-                // 打印arr1
-                // 打印arr2
-                succeed = false;
-                for (int j = 0; j < arr.size(); j++) {
-                    cout << arr[j] <<  " ";
-                }
-                cout << endl;
-                break;
-            }
-        }
-        cout << (succeed? "Nice!" : "Fucking fucked!")<<endl;
-        vector<int> arr = generateRandomArray(maxSize, maxValue);
-        pprint("arr before ",arr);
-        this->functionSort(arr);
-        pprint("arr after",arr);
-    }
-
-};
-
 
 int main() {
-    vector<int> nums4{2, 3, 56, 89, 26, 65, 19, 16, 23};
+    vector<int> nums{5, 2, 25, 65, 89, 55, 59, 32, 4, 1, 3, 20, 6, 11};
+    int k = 4;
 
-    QuickSort quickSort;
+
+    Solution go;
+    auto res = go.findKthLargest(nums, nums.size() / 2);
+    cout << "res  " << res << endl;
+    cout << "nums.size()/2 " << nums.size() / 2 << endl;
+    for (int i = 0; i < nums.size(); ++i) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
 
 
-//    QuickSort::pprint("in", nums4);
-//    // 2*O(n/2)+O(n)  ==> O(n*log(n))
-//    quickSort.quickSort1(nums4);
-//    QuickSort::pprint("out", nums4);
-    quickSort.run();
-//    quickSort
     return 0;
 }
--- 215/setPartation.cpp
+++ 215/setPartation.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+
+
 class Solution {
 public:
 
@@ -43,18 +45,54 @@ public:
     }
 
 
-    int findKthLargest(vector<int> &nums, int k) {
+    int findKthLargest(vector<int> &nums) {
         return quickSelect(nums, 0, nums.size() - 1, nums.size() / 2);
     }
 };
 
+
+class Solution2{
+public:
+    int findKthLargest(vector<int> &arr, int low, int high, int k) {
+        bool flag= true;
+        int low0=0,high0=arr.size()-1;
+        while (flag){
+//            swap(arr[low], arr[low + rand() % (high - low + 1)]);
+            cout << arr[low]<<endl;
+            int pivot = arr[low];
+            while (low < high) {
+                while (low < high && arr[high] >= pivot) high--;
+                arr[low] = arr[high];
+                while (low < high && arr[low] <= pivot) low++;
+                arr[high] = arr[low];
+            }
+            arr[low] = pivot;
+
+            if(low==k){
+                flag= false;
+                return low;
+            }else if(low<k){
+                low0=++low;
+                high=high0;
+
+            }else {
+
+                high0=--high;
+                low=low0;
+            }
+        }
+
+
+    }
+};
+
+
 int main() {
     vector<int> nums{5, 2, 25, 65, 89, 55, 59, 32, 4, 1, 3, 20, 6, 11};
-    int k = 4;
 
 
-    Solution go;
-    auto res = go.findKthLargest(nums, k);
+    Solution2 go;
+    auto res = go.findKthLargest(nums, 0, nums.size()-1,nums.size()/2);
     cout << "res  " << res << endl;
     cout << "nums.size()/2 " << nums.size() / 2 << endl;
     for (int i = 0; i < nums.size(); ++i) {
