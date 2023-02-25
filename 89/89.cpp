cb75a2d6c280ba40fcc6c98ca5fed6dc951bb045
betteryjs <aanbnbnbnb@gmail.com>
2023-02-25 22:34:30 +0800

    add 12347

--- 89/89.cpp
+++ 89/89.cpp
@@ -9,18 +9,43 @@ class Solution {
 public:
     vector<int> grayCode(int n) {
         vector<int> res{0};
-        int head=1;
+        int head = 1;
         for (int i = 0; i < n; ++i) {
-            for (int j = res.size()-1; j >=0; j--) {
-                res.push_back(head+res[j]);
+            for (int j = res.size() - 1; j >= 0; j--) {
+                res.push_back(head + res[j]);
 
             }
-            head=head<<1;
+            head = head << 1;
         }
         return res;
 
 
+    }
+};
+
+
+//* int gray(x) {
+//    return x ^ (x >> 1);
+//}*/
+// /
+// /
+// [0,..2^n−1]
+class Solution2 {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> res{};
+        for (int i = 0; i < pow(2,n); ++i) {
+            res.push_back(gray(i));
+            
+        }
+        return res;
 
 
     }
+
+    int gray(int x) {
+        return x ^ (x >> 1);
+    }
 };
+
+
